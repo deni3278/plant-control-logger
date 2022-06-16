afffec628da6fbdc3239c24d5cd284980c5ddeb3
CKDenni <deni3278@easv365.dk>
2022-06-16 11:43:00 +0200

    Add docstrings

--- air.py
+++ air.py
@@ -6,8 +6,20 @@ _sensor = adafruit_am2320.AM2320(busio.I2C(board.SCL, board.SDA))
 
 
 def humidity() -> float:
+    """
+    Measures the relative air humidity.
+
+    @return: The relative air humidity.
+    """
+
     return round(_sensor.relative_humidity, 2)
 
 
 def temperature() -> float:
+    """
+    Measures the air temperature.
+
+    @return: The air temperature.
+    """
+
     return round(_sensor.temperature, 2)
